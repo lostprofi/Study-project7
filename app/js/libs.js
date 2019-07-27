c2be6ca447229404bfc02b7d05215d1f730f29b6
lostprofi <o-lazarchik@mail.ru>
2019-07-27 23:14:39 +0300

    commit #18

--- app/js/libs.js
+++ app/js/libs.js
@@ -1,226 +1,17 @@
 const template = document.querySelector('.product');
 const btn = document.querySelector('#lazy-btn');
 
-let data = {
-    
-    products: [
-        
-        //group one
-        [
-            {
-                'data-src': '#modal-9',
-                src: 'img/astral-cruise.png',
-                alt: 'astral',
-                class: 'astral',
-                id: 'modal-9',
-                productName: 'Astral Cruise',
-                price1: 6,
-                price2: 5
-            
-            },
-            
-            {
-                'data-src': '#modal-10',
-                src: 'img/symbolic-bag.png',
-                alt: 'symbolic bag',
-                class: 'symb-bag',
-                id: 'modal-10',
-                productName: 'Symbolic Bag',
-                price1: 9,
-                price2: 1
-            
-            },
-            
-            {
-                'data-src': '#modal-11',
-                src: 'img/onesie.png',
-                alt: 'onesie',
-                class: 'onesie',
-                id: 'modal-11',
-                productName: 'Onesie',
-                price1: 5,
-                price2: 7
-            
-            },
-            
-            {
-                'data-src': '#modal-12',
-                src: 'img/bite-me.png',
-                alt: 'bite',
-                class: 'bite',
-                id: 'modal-12',
-                productName: 'Bite Me',
-                price1: 6,
-                price2: 7
-            
-            }
-            
-        ],
+let data = '{ "products": [ [ {"data-src": "#modal-9", "src": "img/astral-cruise.png", "alt": "astral", "class": "astral", "id": "modal-9", "productName": "Astral Cruise", "price1": 6, "price2": 5 }, { "data-src": "#modal-10", "src": "img/symbolic-bag.png", "alt": "symbolic bag", "class": "symb-bag", "id": "modal-10", "productName": "Symbolic Bag", "price1": 9, "price2": 1 }, { "data-src": "#modal-11", "src": "img/onesie.png", "alt": "onesie", "class": "onesie", "id": "modal-11", "productName": "Onesie", "price1": 5, "price2": 7 }, { "data-src": "#modal-12", "src": "img/bite-me.png", "alt": "bite", "class": "bite", "id": "modal-12", "productName": "Bite Me", "price1": 6, "price2": 7 } ], [ { "data-src": "#modal-13", "src": "img/mascot-kitty.png", "alt": "kitty", "class": "kitty", "id": "modal-13", "productName": "Mascot Kitty - White", "price1": 2, "price2": 3 }, { "data-src": "#modal-14", "src": "img/Little-fella.png", "alt": "little fella", "class": "fella", "id": "modal-14", "productName": "Little Fella", "price1": 3, "price2": 4 }, {"data-src": "#modal-15", "src": "img/interesting-read.png", "alt": "int-read", "class": "int-read", "id": "modal-15", "productName": "Interesting Read", "price1": 5, "price2": 1 }, { "data-src": "#modal-16", "src": "img/the-carter.png", "alt": "the carter", "class": "carter", "id": "modal-16", "productName": "The Carter", "price1": 7, "price2": 1 } ], [ { "data-src": "#modal-17", "src": "img/the-carter.png", "alt": "the carter", "class": "carter", "id": "modal-17", "productName": "The Carter", "price1": 4, "price2": 3 }, { "data-src": "#modal-18", "src": "img/mascot-kitty.png", "alt": "kitty", "class": "kitty", "id": "modal-18", "productName": "Mascot Kitty -White", "price1": 6, "price2": 1 }, { "data-src": "#modal-19", "src": "img/Little-fella.png", "alt": "little fella", "class": "fella", "id": "modal-19", "productName": "Little Fella", "price1": 5, "price2": 8 }, { "data-src": "#modal-20", "src": "img/bite-me.png", "alt": "bite", "class": "bite", "id": "modal-20", "productName": "Bite Me", "price1": 7, "price2": 1 } ], [ { "data-src": "#modal-21", "src": "img/bite-me.png", "alt": "bite", "class": "bite", "id": "modal-21", "productName": "Bite Me", "price1": 4, "price2": 6 }, { "data-src": "#modal-22", "src": "img/Little-fella.png", "alt": "little fella", "class": "fella", "id": "modal-22", "productName": "Little Fella", "price1": 8, "price2": 1 }, {"data-src": "modal-23", "src": "img/the-carter.png", "alt": "the carter", "class": "carter", "id": "modal-23", "productName": "The Carter", "price1": 9, "price2": 5 }, { "data-src": "#modal-24", "src": "img/onesie.png", "alt": "onesie", "class": "onesie", "id": "modal-24", "productName": "Onesie", "price1": 7, "price2": 7 } ] ] }';
 
-        [
-            {
-                'data-src': '#modal-13',
-                src: 'img/mascot-kitty.png',
-                alt: 'kitty',
-                class: 'kitty',
-                id: 'modal-13',
-                productName: 'Mascot Kitty - White',
-                price1: 2,
-                price2: 3
-
-            },
-
-            {
-                'data-src': '#modal-14',
-                src: 'img/Little-fella.png',
-                alt: 'little fella',
-                class: 'fella',
-                id: 'modal-14',
-                productName: 'Little Fella',
-                price1: 3,
-                price2: 4
-
-            },
-
-            {
-                'data-src': '#modal-15',
-                src: 'img/interesting-read.png',
-                alt: 'int-read',
-                class: 'int-read',
-                id: 'modal-15',
-                productName: 'Interesting Read',
-                price1: 5,
-                price2: 1
-
-            },
-
-            {
-                'data-src': '#modal-16',
-                src: 'img/the-carter.png',
-                alt: 'the carter',
-                class: 'carter',
-                id: 'modal-16',
-                productName: 'The Carter',
-                price1: 7,
-                price2: 1
-
-            }
-        ],
-        
-        [
-            {
-                'data-src': '#modal-17',
-                src: 'img/the-carter.png',
-                alt: 'the carter',
-                class: 'carter',
-                id: 'modal-17',
-                productName: 'The Carter',
-                price1: 4,
-                price2: 3
-
-            },
-
-            {
-                'data-src': '#modal-18',
-                src: 'img/mascot-kitty.png',
-                alt: 'kitty',
-                class: 'kitty',
-                id: 'modal-18',
-                productName: 'Mascot Kitty -White',
-                price1: 6,
-                price2: 1
-
-            },
-
-            {
-                'data-src': '#modal-19',
-                src: 'img/Little-fella.png',
-                alt: 'little fella',
-                class: 'fella',
-                id: 'modal-19',
-                productName: 'Little Fella',
-                price1: 5,
-                price2: 8
-
-            },
-
-            {
-                'data-src': '#modal-20',
-                src: 'img/bite-me.png',
-                alt: 'bite',
-                class: 'bite',
-                id: 'modal-20',
-                productName: 'Bite Me',
-                price1: 7,
-                price2: 1
-
-            }
-        ],
-        
-        [
-            {
-                'data-src': '#modal-21',
-                src: 'img/bite-me.png',
-                alt: 'bite',
-                class: 'bite',
-                id: 'modal-21',
-                productName: 'Bite Me',
-                price1: 4,
-                price2: 6
-            },
-
-            {
-                'data-src': '#modal-22',
-                src: 'img/Little-fella.png',
-                alt: 'little fella',
-                class: 'fella',
-                id: 'modal-22',
-                productName: 'Little Fella',
-                price1: 8,
-                price2: 1
-
-            },
-
-            {
-                'data-src': '#modal-23',
-                src: 'img/the-carter.png',
-                alt: 'the carter',
-                class: 'carter',
-                id: 'modal-23',
-                productName: 'The Carter',
-                price1: 9,
-                price2: 5
-
-            },
-
-            {
-                'data-src': '#modal-24',
-                src: 'img/onesie.png',
-                alt: 'onesie',
-                class: 'onesie',
-                id: 'modal-24',
-                productName: 'Onesie',
-                price1: 7,
-                price2: 7
-
-            }
-        ]
-        
-        
-        
-        
-    ]
-      
-};
+data = JSON.parse(data);
 
 let i = 0;
 
-
-
-
 const lazyLoad = ()=>{
     
-try {   
+try { 
+    
+    
     
      data.products[i].forEach(el => {
     
@@ -402,7 +193,7 @@ let tglDownLtstFunc = () => {
     }
 
 
-console.log(plusMassive);
+
 
 btnTglFtrd.addEventListener('click', tglDownFtrdFunc);
 btnTglLtst.addEventListener('click', tglDownLtstFunc);
